0bb940a2e2e7912139ddcd6738084b5b32f334b9
shrawan <agrawalshrawan245@gmail.com>
2023-08-03 19:44:59 +0900

    security config

--- src/main/java/com/agrawals/blogger/security/service/CustomUserDetailsService.java
+++ src/main/java/com/agrawals/blogger/security/service/CustomUserDetailsService.java
@@ -3,7 +3,6 @@ package com.agrawals.blogger.security.service;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
